7d6429458bfbb2516e296784fa93a2956675ae90
Beryl-webdev <akinyib927@gmail.com>
2025-05-31 01:09:29 +0300

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
 const myVar = "C is fun";
 console.log(myVar);
-const myVar = "Python is cool";
+myVar = "Python is cool";
 console.log(myVar);
-const myVar = "Javascript is amazing";
+myVar = "Javascript is amazing";
 console.log(myVar);
