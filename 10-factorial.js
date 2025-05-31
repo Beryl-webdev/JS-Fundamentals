954f09a62f0ecf6b3a945b401a515b194197a9ff
Beryl-webdev <akinyib927@gmail.com>
2025-05-31 03:04:54 +0300

    Create 10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+function factorial(n) {
+  if (isNaN(n)) return 1;
+  n = parseInt(n);
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+const arg = process.argv[2];
+console.log(factorial(arg));
